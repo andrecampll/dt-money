2bcbd4d302f0c3c5aae54ba7f43217baa1310047
andrecampll <andrevictor50@gmail.com>
2022-09-06 06:34:15 -0300

    feat: adding transactions hook and context 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider, GlobalStyle } from '@chakra-ui/react'
+import { TransactionsProvider } from './hooks/useTransactions'
 import { Transactions } from './pages/Transactions/Transactions'
 
 import { theme } from './styles/themes/default'
 
 export const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <GlobalStyle />
-      <Transactions />
-    </ChakraProvider>
+    <TransactionsProvider>
+      <ChakraProvider theme={theme}>
+        <GlobalStyle />
+        <Transactions />
+      </ChakraProvider>
+    </TransactionsProvider>
   )
 }
